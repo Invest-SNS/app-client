bda7abb485343fa8d11c96ef568b01152485a44f
yjp8842 <yjp20183029@gmail.com>
2024-03-11 14:44:21 +0900

    feat: react-redux 환경 세팅

---
+++ src/store/reducers/Trading/trading.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  
+};
+
+const tradingSlice = createSlice({
+  name: "trading",
+  initialState: initialState,
+  reducers: {
+  },
+  extraReducers: (builder) => {
+  },
+});
+
+export default tradingSlice.reducer;
