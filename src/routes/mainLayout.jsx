ab66a028bc5175d411f895141747ed4082d55dd7
eunxoo <dmstn7989@naver.com>
2024-03-12 17:03:28 +0900

    feat: 네비바 구현

--- src/routes/mainLayout.jsx
+++ src/routes/mainLayout.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import MyNavbar from "../components/MyNavbar";
 
 export default function MainLayout() {
   return (
     <>
+      <MyNavbar />
       <Outlet />
     </>
-  )
+  );
 }
