346f48ba6bc5ac8ba0664d3570a2049a6ac200ce
eunxoo <dmstn7989@naver.com>
2024-03-14 09:47:43 +0900

    feat: 지표 변수 디테일바 구현 및 리덕스 연결

---
+++ src/store/reducers/Trading/chartValues.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  values: {
+    SMA: [5, 20, 60, 120, 240],
+    WMA: [5, 20, 60, 120, 240],
+    EMA: [5, 20, 60, 120, 240],
+    BBANDS: [20, 2],
+    SAR: [0.02, 0.2],
+  },
+};
+
+const chartValuesSlice = createSlice({
+  name: "hartValues",
+  initialState: initialState,
+  reducers: {
+    setValues: (state, action) => {
+      state.values = { ...state.values, ...action.payload };
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { setValues } = chartValuesSlice.actions;
+export default chartValuesSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,45 @@
-import React from 'react';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
-
-import tradingReducer from './reducers/Trading/trading';
+import React from "react";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import logger from "redux-logger";
+import tradingReducer from "./reducers/Trading/trading";
+import chartValuesReducer from "./reducers/Trading/chartValues";
+import indicatorValuesReducer from "./reducers/Trading/indicatorValues";
 
 const rootPersistConfig = {
-  key: 'root',
+  key: "root",
   storage: storage,
-  whitelist: []
-}
+  // whitelist: ["chartValues", "indicatorValues"],
+};
 
 const rootReducer = persistReducer(
   rootPersistConfig,
   combineReducers({
     // 임시 reducer
     trading: tradingReducer,
+    chartValues: chartValuesReducer,
+    indicatorValues: indicatorValuesReducer,
   })
 );
-
+const myMiddlewares = [logger];
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }),
+    }).concat(myMiddlewares),
 });
 
 export const persistor = persistStore(store);
