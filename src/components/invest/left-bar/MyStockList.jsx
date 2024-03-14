5a376533984ef0d77367798cbb43574cb31f73a9
yjp8842 <yjp20183029@gmail.com>
2024-03-14 10:51:18 +0900

    feat: left-bar mainLayout으로 이동

--- src/components/invest/left-bar/MyStockList.jsx
+++ src/components/invest/left-bar/MyStockList.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import clickCompany, { setClickCompany } from "../../../store/reducers/Chart/clickCompany";
 
 const MyStockList = () => {
   const [showSearch, setShowSearch] = useState(false);
@@ -114,6 +116,8 @@ const MyStockList = () => {
     }
   };
 
+  const dispatch = useDispatch();
+
   return (
     <LeftContainer>
       <MyItemContainer>
@@ -153,7 +157,10 @@ const MyStockList = () => {
               {searchResults.length > 0 && (
                 <SearchResults>
                   {searchResults.map((result) => (
-                    <SearchResult key={result.id}>
+                    <SearchResult key={result.id} onClick={() => {
+                      console.log('')
+                      dispatch(setClickCompany(result))
+                    }}>
                       <img
                         src={`https://file.alphasquare.co.kr/media/images/stock_logo/kr/${result.code}.png`}
                         style={{
@@ -233,7 +240,7 @@ const MyStockList = () => {
 };
 
 const LeftContainer = styled.section`
-  width: 250px;
+  min-width: 250px;
   height: calc(100vh - 57px);
   border-right: 1px solid #e2e2e2;
 `;
