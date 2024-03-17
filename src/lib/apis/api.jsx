264c76ab552e869268fa565a436ef537e631475f
eunxoo <dmstn7989@naver.com>
2024-03-18 08:23:58 +0900

    feat: 검색 api 연결 및 리덕스 상태 관리
    로그인은 관심종목 테스트 위해 임시로 연결했어요

---
+++ src/lib/apis/api.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { getCookie } from "./cookie";
+
+export const BASE_URL = "http://127.0.0.1:3000/api";
+
+export const chartInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const subChartInstance = axios.create({
+  baseURL: BASE_URL + "/subChart",
+});
+
+export const baseInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const baseUserInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${getCookie("token")}`,
+  },
+});
--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
+import React from "react";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import logger from "redux-logger";
+import tradingReducer from "./reducers/Trading/trading";
+import chartValuesReducer from "./reducers/Trading/chartValues";
+import indicatorValuesReducer from "./reducers/Trading/indicatorValues";
+import chartReducer from "./reducers/Chart/chart.jsx";
+import searchReducer from "./reducers/Trading/search";
+import userReducer from "./reducers/User/user";
 
 import tradingReducer from './reducers/Trading/trading';
 
 const rootPersistConfig = {
   key: 'root',
   storage: storage,
-  whitelist: []
-}
+  whitelist: ["user", "chartValues", "indicatorValues", "search"],
+};
 
 const rootReducer = persistReducer(
   rootPersistConfig,
   combineReducers({
     // 임시 reducer
     trading: tradingReducer,
+    chart: chartReducer,
+    chartValues: chartValuesReducer,
+    indicatorValues: indicatorValuesReducer,
+    search: searchReducer,
+    user: userReducer,
   })
 );
 
