e51a3da27fd8a58717f4ea2a2b249b67d46a2506
yjp8842 <yjp20183029@gmail.com>
2024-03-19 11:49:10 +0900

    feat: RSIChart 구현

---
+++ src/components/invest/chart/Indicators/sub/RSIChart.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import { BarSeries, Chart, LineSeries, MACDSeries, XAxis, YAxis } from 'react-financial-charts';
+import { useDispatch, useSelector } from 'react-redux';
+import { setChartDatas } from '../../../../../store/reducers/Chart/chart';
+import { getMACDChart, getRSIChart, getSTOCHChart, getSTOCHFChart } from '../../../../../store/reducers/Chart/Indicators/sub';
+import { format } from 'd3-format';
+
+export default function RSIChart({ datas }) {
+  const dispatch = useDispatch();
+  const isActive = useSelector((state) => state.clickIndicator.RSI);
+  const RSIValue = useSelector((state) => state.indicatorValues.values.RSI);
+
+  const calculateRSI = (data) => {
+    const updatedDatas = datas.map(item => {
+      const newItem = { ...item };
+      delete newItem.rsi;
+      return newItem;
+    });
+
+    const newData = [...updatedDatas];
+    
+    const f_idx = data.begIndex;
+    const l_idx = data.nbElement;
+    const rsi = data.result.outReal;
+    for (let i = 0; i < l_idx; i++) {
+      newData[f_idx + i] = {
+        ...newData[f_idx + i], // 기존 객체를 복사
+        ["rsi"]: rsi[i], // 새로운 속성 추가
+      };
+    }
+    dispatch(setChartDatas(newData));
+  }
+
+  useEffect(() => {
+    if (isActive) {
+      const data = {
+        "chart": datas,
+        "Date" : RSIValue[0],
+      }
+      dispatch(getRSIChart(data))
+        .then((res) =>  calculateRSI(res.payload))
+    } else if (!isActive) {
+      const updatedDatas = datas.map(item => {
+        const newItem = { ...item };
+        delete newItem.rsi;
+        return newItem;
+      });
+      dispatch(setChartDatas(updatedDatas));
+    }
+  }, [isActive]);
+
+  const pricesDisplayFormat = format(".2f");
+
+  return (
+    <>
+      <XAxis showGridLines gridLinesStrokeStyle="#e0e3eb" />
+      <YAxis ticks={4} tickFormat={pricesDisplayFormat} />
+      <LineSeries yAccessor={d => d.rsi} strokeStyle='#680A08' />
+    </>
+  )
+}
