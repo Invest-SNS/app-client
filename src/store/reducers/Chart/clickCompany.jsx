09ba7ddd0f44e4bb95830214988e4677d6bd9adc
yjp8842 <yjp20183029@gmail.com>
2024-03-20 08:50:19 +0900

    fix: 오류 수정

--- src/store/reducers/Chart/clickCompany.jsx
+++ src/store/reducers/Chart/clickCompany.jsx
@@ -1,7 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  data: {}
+  data: {
+    code: "005930",
+    market: "kospi",
+    market_code: "KR7005930003",
+    name: "삼성전자",
+    __v: 0,
+    _id: "65f24176b5fd9b9fdc483b63",
+  }
 };
 
 const companySlice = createSlice({
