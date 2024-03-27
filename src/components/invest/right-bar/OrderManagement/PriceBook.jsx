9c2c5502ba3e563364b61db336eecf21726ca156
chaeheonjeong <cogjs3210@naver.com>
2024-03-27 14:39:31 +0900

    feat: order

--- src/components/invest/right-bar/OrderManagement/PriceBook.jsx
+++ src/components/invest/right-bar/OrderManagement/PriceBook.jsx
@@ -16,16 +16,6 @@ const PriceBook = () => {
     (state) => state.trading
   );
 
-  // const askPrice = {
-  //   message: {
-  //     sellPrice: [100, 101, 102, 103, 104, 104, 104, 104, 104, 104],
-  //     sellAmount: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-  //     buyPrice: [100, 101, 102, 103, 104, 104, 104, 104, 104, 104],
-  //     buyAmount: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-  //   },
-  // };
-  // const nowPrice = 100;
-
   useEffect(() => {
     if (containerRef.current) {
       containerRef.current.scrollTop = scrollPosition;
