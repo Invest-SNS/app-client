74a9e7e035b8c1c2f01387ff811164bca57901e4
eunxoo <dmstn7989@naver.com>
2024-03-29 13:33:14 +0900

    feat: 로그인 로그아웃

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
-import { Container, Navbar, Nav, Offcanvas, Button, Modal } from "react-bootstrap";
+import {
+  Container,
+  Navbar,
+  Nav,
+  Offcanvas,
+  Button,
+  Modal,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import ChatBot from "../routes/chatBot/chatBot";
-import LogoIcon from '../../public/icon/logo.svg'
+import LogoIcon from "../../public/icon/logo.svg";
 import { postLogout, setUser } from "../store/reducers/User/user";
-import chatbotImg from '../../public/icon/chat_mate.jpg';
+import chatbotImg from "../../public/icon/chat_mate.jpg";
 import styled from "styled-components";
-
+import { getCookie } from "../lib/apis/cookie";
 
 const EXPAND_BREAKPOINT = "md";
 
 const MyNavbar = ({ offCanvasTitle }) => {
   const [showChatBot, setShowChatBot] = useState(false); // ChatBot 표시 상태
-  const toggleChatBot = () => setShowChatBot(prev => !prev);
+  const toggleChatBot = () => setShowChatBot((prev) => !prev);
   const [logoutModal, setLogoutModal] = useState(false);
   const User = useSelector((state) => state.user.user);
+
   const dispatch = useDispatch();
-  
+  const isLogin = !!getCookie("token");
+  const token = getCookie("token");
   return (
     <Navbar
       expand={EXPAND_BREAKPOINT}
@@ -25,7 +34,15 @@ const MyNavbar = ({ offCanvasTitle }) => {
       style={{ borderBottom: "1px solid black" }}
     >
       <Container fluid>
-        <Navbar.Brand href="/" style={{ display: 'flex', gap: '10px', alignItems: 'center', fontWeight: '300' }}>
+        <Navbar.Brand
+          href="#"
+          style={{
+            display: "flex",
+            gap: "10px",
+            alignItems: "center",
+            fontWeight: "100",
+          }}
+        >
           <img src={LogoIcon} width={37} />
           StockMate
         </Navbar.Brand>
@@ -44,15 +61,21 @@ const MyNavbar = ({ offCanvasTitle }) => {
             <Nav
               className={`gap-2 justify-content-around flex-row pb-4 pb-${EXPAND_BREAKPOINT}-0`}
             >
-              {!User?.token ? (
+              {!isLogin ? (
                 <div style={{ display: "flex", gap: "15px" }}>
-                
                   {/* 챗봇 토글 버튼 */}
                   <ChatBotBtn onClick={toggleChatBot}>
-                    <img src={chatbotImg} alt="Robot" style={{ width: '40px', height: '40px', borderRadius: '20px' }}/>
-                    <span style={{ fontWeight: 500 }}>ChatMate</span>
+                    <img
+                      src={chatbotImg}
+                      alt="Robot"
+                      style={{
+                        width: "40px",
+                        height: "40px",
+                        borderRadius: "20px",
+                      }}
+                    />
+                    <span>ChatBot</span>
                   </ChatBotBtn>
-
                   <Nav.Link
                     as={Link}
                     className="flex-grow-1 text-center"
@@ -68,12 +91,20 @@ const MyNavbar = ({ offCanvasTitle }) => {
                     회원가입
                   </Nav.Link>
                 </div>
-              ) : ( 
+              ) : (
                 <>
                   {/* 챗봇 토글 버튼 */}
                   <ChatBotBtn onClick={toggleChatBot}>
-                    <img src={chatbotImg} alt="Robot" style={{ width: '40px', height: '40px', borderRadius: '20px' }}/>
-                    <span>ChatMate</span>
+                    <img
+                      src={chatbotImg}
+                      alt="Robot"
+                      style={{
+                        width: "40px",
+                        height: "40px",
+                        borderRadius: "20px",
+                      }}
+                    />
+                    <span>ChatBot</span>
                   </ChatBotBtn>
                   <Nav.Link
                     as="div"
@@ -92,7 +123,12 @@ const MyNavbar = ({ offCanvasTitle }) => {
                 </>
               )}
               {/* ChatBot 모달 */}
-              <Modal show={showChatBot} onHide={toggleChatBot} size="lg" centered>
+              <Modal
+                show={showChatBot}
+                onHide={toggleChatBot}
+                size="lg"
+                centered
+              >
                 <Modal.Header closeButton>
                   <Modal.Title>ChatMate</Modal.Title>
                 </Modal.Header>
@@ -112,18 +148,26 @@ const MyNavbar = ({ offCanvasTitle }) => {
                   <span>로그아웃 하시겠습니까?</span>
                 </Modal.Body>
                 <Modal.Footer>
-                  <Button style={{ backgroundColor: '#6e97ff', border: '1px solid #6e97ff' }} onClick={() => {
-                    dispatch(postLogout(User.token))
-                      .then((res) => {
+                  <Button
+                    style={{
+                      backgroundColor: "#6e97ff",
+                      border: "1px solid #6e97ff",
+                    }}
+                    onClick={() => {
+                      dispatch(postLogout(token)).then((res) => {
                         if (res.payload.status === 200) {
-                          dispatch(setUser({}))
-                          setLogoutModal(false)
+                          dispatch(setUser({}));
+                          setLogoutModal(false);
                         }
-                      })
-                  }}>
+                      });
+                    }}
+                  >
                     네
                   </Button>
-                  <Button variant="secondary" onClick={() => setLogoutModal(false)}>
+                  <Button
+                    variant="secondary"
+                    onClick={() => setLogoutModal(false)}
+                  >
                     아니오
                   </Button>
                 </Modal.Footer>
@@ -154,4 +198,4 @@ const ChatBotBtn = styled.div`
       color: #ff8b5c;
     }
   }
-`
+`;
